4afe7b932c53b9a7899cd1916add0542111d5163
upendra <upendrarajput9911@gmail.com>
2021-10-18 15:33:55 +0530

    block-BJaay

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -3,11 +3,31 @@
 // Do the following using loops:
 
 // Using `console.log` log all the values from 1 to 10.
+for(let i = 1; i <= 10; i = i + 1){
+    console.log("Hellow World")
+}
 
 // Using `console.log` log all the evan values from 1 to 10.
-
+for(let i = 1; i <= 10; i = i + 1){
+    if(i % 2 ===0){
+        alert(`The number is even`)
+    }
+    console.log("Hellow World")
+}
 // Using `console.log` log all the od values from 1 to 10.
-
+for(let i = 1; i <= 10; i = i + 1){
+    if(i % 2 !==0){
+        alert(`The number is odd`)
+    }
+    console.log("Hellow World")
+}
 // Calculate the sum of all numbers from 1 to 10.
-
+for(let i = 1; i <= 10; i=i+1)
+{
+alert(5*(i+10))
+    
+}
 // Log all the values from 1 to 10 using while loop
+for (let i=10; i>0; i=i-1){
+    console.log("Hellow World")
+}
